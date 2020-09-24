79b5e1ae3ced5bc1b194134cf0b13abbf45d9e10
gylim0604 <gylim0604@hotmail.com>
2020-09-24 12:12:11 -0500

    added trash icon and strikethrough effect: '

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config
 from flask_bootstrap import Bootstrap
+from flask_fontawesome import FontAwesome
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 bootstrap = Bootstrap(app)
+fa = FontAwesome(app)
 
 from app import routes, models
